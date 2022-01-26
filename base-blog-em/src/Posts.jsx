5b6b769433ea67670f6f7776accc793d436e7552
WhatIfWeDigDeeper <1247548+WhatIfWeDigDeeper@users.noreply.github.com>
2022-01-26 08:00:06 -0600

    2.11 pagination

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -4,21 +4,23 @@ import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
+const fakeApiStartPage = 1;
+
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(fakeApiStartPage);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
   const { data, error, isError, isLoading } = useQuery(
-    "posts",
-    fetchPosts,
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
     { staleTime: 2000 }
   );
 
@@ -45,11 +47,18 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= fakeApiStartPage} onClick={() => {
+          setCurrentPage((previousValue) => previousValue - 1);
+        }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
